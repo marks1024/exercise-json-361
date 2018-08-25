26a178859f8bd8e17610d2be33bd86429268560b
Alibek Manabayev <Alibek.manabayev@gmail.com>
2018-08-25 12:55:29 +0300

    add: ContactList

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -1,37 +1,37 @@
 package kz.edu.nu.cs.exercise;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactList {
     private List<MyContact> list;
-    
+
     public ContactList() {
         list = new ArrayList<MyContact>();
     }
-    
+
     public static ContactList MakeContactList(String s) {
-        // Complete this method, use Gson
-        return null;
+        Gson gson = new Gson();
+        return gson.fromJson(s, ContactList.class);
     }
-    
+
     public void addContact(MyContact c) {
         list.add(c);
     }
-    
+
     public void removeFirstContact() {
         list.remove(0);
     }
-    
+
     public MyContact getFirstContact() {
-        // complete this method
-        // return correct value
-        return null;
+//        if (list.isEmpty())
+//            return null;
+        return list.get(0);
     }
-    
+
     public int getSize() {
-        // complete this method
-        // return correct value
-        return 0;
+        return list.size();
     }
 }
