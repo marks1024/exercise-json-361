4331539c30880ee516b31a8931e008f472ccd8c0
This.is.MUHA <mukhit.yelemes@nu.edu.kz>
2018-08-25 16:36:10 +0600

    Well done

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -2,6 +2,7 @@ package kz.edu.nu.cs.exercise;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.*;
 
 public class ContactList {
     private List<MyContact> list;
@@ -11,8 +12,9 @@ public class ContactList {
     }
     
     public static ContactList MakeContactList(String s) {
-        // Complete this method, use Gson
-        return null;
+        Gson gson = new Gson();
+        ContactList contactList = gson.fromJson(s, ContactList.class);
+        return contactList;
     }
     
     public void addContact(MyContact c) {
@@ -24,14 +26,10 @@ public class ContactList {
     }
     
     public MyContact getFirstContact() {
-        // complete this method
-        // return correct value
-        return null;
+        return list.get(0);
     }
     
     public int getSize() {
-        // complete this method
-        // return correct value
-        return 0;
+        return list.size();
     }
 }
