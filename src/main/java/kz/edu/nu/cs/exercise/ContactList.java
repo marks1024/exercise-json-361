bf3666f2cecb343e7dcd61cd2aeb08cc2b2514ba
marks <marks1024@users.noreply.github.com>
2018-08-25 13:00:12 +0600

    first commit

---
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -0,0 +1,37 @@
+package kz.edu.nu.cs.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactList {
+    private List<MyContact> list;
+    
+    public ContactList() {
+        list = new ArrayList<MyContact>();
+    }
+    
+    public static ContactList MakeContactList(String s) {
+        // Complete this method, use Gson
+        return null;
+    }
+    
+    public void addContact(MyContact c) {
+        list.add(c);
+    }
+    
+    public void removeFirstContact() {
+        list.remove(0);
+    }
+    
+    public MyContact getFirstContact() {
+        // complete this method
+        // return correct value
+        return null;
+    }
+    
+    public int getSize() {
+        // complete this method
+        // return correct value
+        return 0;
+    }
+}
---
+++ src/main/java/kz/edu/nu/cs/exercise/MyContact.java
@@ -0,0 +1,42 @@
+package kz.edu.nu.cs.exercise;
+
+public class MyContact {
+    private String name;
+    private int age;
+    private String number;
+    
+    public MyContact(String name, int age, String number) {
+        this.name = name;
+        this.age = age;
+        this.number = number;
+    }
+    
+    public static MyContact MakeMyContact(String s) {
+        // complete this method, use Gson
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
