6da60be3f158f5b211e27bc5adbc79330cbc981f
Dana <dana.zhumabekova@nu.edu.kz>
2018-08-25 15:39:55 +0600

    some changes

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -2,6 +2,9 @@ package kz.edu.nu.cs.exercise;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.*;
+
+
 
 public class ContactList {
     private List<MyContact> list;
@@ -11,8 +14,9 @@ public class ContactList {
     }
     
     public static ContactList MakeContactList(String s) {
+        Gson gson = new Gson ();
         // Complete this method, use Gson
-        return null;
+        return gson.fromJson(s, ContactList.class);
     }
     
     public void addContact(MyContact c) {
@@ -26,12 +30,12 @@ public class ContactList {
     public MyContact getFirstContact() {
         // complete this method
         // return correct value
-        return null;
+        return list.get(0);
     }
     
     public int getSize() {
         // complete this method
         // return correct value
-        return 0;
+        return list.size();
     }
 }
