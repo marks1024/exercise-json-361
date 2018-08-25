707dceaaf209399d4c6a873d99adf303134a4c30
aikoshago <aidana.nurakhmetova@nu.edu.kz>
2018-08-25 15:39:36 +0600

    aa

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -3,6 +3,8 @@ package kz.edu.nu.cs.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 public class ContactList {
     private List<MyContact> list;
     
@@ -12,7 +14,9 @@ public class ContactList {
     
     public static ContactList MakeContactList(String s) {
         // Complete this method, use Gson
-        return null;
+    	Gson gson = new Gson();
+    	ContactList jsonObj =gson.fromJson(s, ContactList.class);
+        return jsonObj;
     }
     
     public void addContact(MyContact c) {
@@ -26,12 +30,12 @@ public class ContactList {
     public MyContact getFirstContact() {
         // complete this method
         // return correct value
-        return null;
+        return list.get(0);
     }
     
     public int getSize() {
         // complete this method
         // return correct value
-        return 0;
+        return list.size();
     }
 }
