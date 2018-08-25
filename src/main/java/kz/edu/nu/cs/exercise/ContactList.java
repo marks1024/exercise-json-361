b198f0f47ebf275a7442a28209c3be1d66d3bbb0
daniyar.kazbek@nu.edu.kz <daniyar.kazbek@nu.edu.kz>
2018-08-25 14:57:23 +0600

    2nd commit

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -1,5 +1,8 @@
 package kz.edu.nu.cs.exercise;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +14,9 @@ public class ContactList {
     }
     
     public static ContactList MakeContactList(String s) {
-        // Complete this method, use Gson
-        return null;
+        Gson gson = new Gson();
+        ContactList cl = gson.fromJson(s, ContactList.class);
+        return cl;
     }
     
     public void addContact(MyContact c) {
@@ -24,14 +28,10 @@ public class ContactList {
     }
     
     public MyContact getFirstContact() {
-        // complete this method
-        // return correct value
-        return null;
+        return list.get(0);
     }
     
     public int getSize() {
-        // complete this method
-        // return correct value
-        return 0;
+        return list.size();
     }
 }
