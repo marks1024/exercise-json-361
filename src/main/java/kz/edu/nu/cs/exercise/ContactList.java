e5a156dfd601d63f9b622159ac308a43bcfb605c
Baktybek Doskul <baktybek.doskul@nu.edu.kz>
2018-08-25 14:31:51 +0600

    completed the task

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -3,6 +3,8 @@ package kz.edu.nu.cs.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 public class ContactList {
     private List<MyContact> list;
     
@@ -11,8 +13,7 @@ public class ContactList {
     }
     
     public static ContactList MakeContactList(String s) {
-        // Complete this method, use Gson
-        return null;
+    	return (new Gson()).fromJson(s, ContactList.class);
     }
     
     public void addContact(MyContact c) {
@@ -24,14 +25,10 @@ public class ContactList {
     }
     
     public MyContact getFirstContact() {
-        // complete this method
-        // return correct value
-        return null;
+        return list.get(0);
     }
     
     public int getSize() {
-        // complete this method
-        // return correct value
-        return 0;
+        return list.size();
     }
 }
