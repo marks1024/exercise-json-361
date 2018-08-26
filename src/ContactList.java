1683af5def3f284d1838818a1b8835d9371337df
dianazhanseit <32613789+dianazhanseit@users.noreply.github.com>
2018-08-26 18:04:39 +0600

    Add files via upload

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,38 +1,37 @@
 package kz.edu.nu.cs.exercise;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactList {
     private List<MyContact> list;
-    
+
     public ContactList() {
         list = new ArrayList<MyContact>();
     }
-    
+
     public static ContactList MakeContactList(String s) {
         Gson gson = new Gson();
         ContactList g = gson.fromJson(s, ContactList.class);
         return g;
     }
-    
+
+
     public void addContact(MyContact c) {
         list.add(c);
     }
-    
+
     public void removeFirstContact() {
         list.remove(0);
     }
-    
+
     public MyContact getFirstContact() {
-        // complete this method
-        // return correct value
         return list.get(0);
     }
-    
+
     public int getSize() {
-        // complete this method
-        // return correct value
         return list.size();
     }
 }
--- src/MyContact.java
+++ src/MyContact.java
@@ -1,16 +1,18 @@
 package kz.edu.nu.cs.exercise;
 
+import com.google.gson.Gson;
+
 public class MyContact {
     private String name;
     private int age;
     private String number;
-    
+
     public MyContact(String name, int age, String number) {
         this.name = name;
         this.age = age;
         this.number = number;
     }
-    
+
     public static MyContact MakeMyContact(String s) {
         Gson gson = new Gson();
         MyContact g = gson.fromJson(s, MyContact.class);
@@ -41,3 +43,4 @@ public class MyContact {
         this.number = number;
     }
 }
+
