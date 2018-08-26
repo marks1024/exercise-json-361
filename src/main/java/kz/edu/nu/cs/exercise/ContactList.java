ebfa80cb607c995676f57c8a5ac176a9d5a5838d
makisheva <zaida.makisheva@nu.edu.kz>
2018-08-26 21:31:00 +0600

    my channges

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -3,6 +3,8 @@ package kz.edu.nu.cs.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 public class ContactList {
     private List<MyContact> list;
     
@@ -12,6 +14,8 @@ public class ContactList {
     
     public static ContactList MakeContactList(String s) {
         // Complete this method, use Gson
+    	Gson gson = new Gson();
+    	String json = gson.toJson(s);
         return null;
     }
     
@@ -26,12 +30,14 @@ public class ContactList {
     public MyContact getFirstContact() {
         // complete this method
         // return correct value
+    	list.get(0);
         return null;
     }
     
     public int getSize() {
         // complete this method
         // return correct value
+    	list.size();
         return 0;
     }
 }
--- src/main/java/kz/edu/nu/cs/exercise/MyContact.java
+++ src/main/java/kz/edu/nu/cs/exercise/MyContact.java
@@ -1,5 +1,7 @@
 package kz.edu.nu.cs.exercise;
 
+import com.google.gson.Gson;
+
 public class MyContact {
     private String name;
     private int age;
@@ -13,6 +15,8 @@ public class MyContact {
     
     public static MyContact MakeMyContact(String s) {
         // complete this method, use Gson
+    	Gson gson = new Gson();
+    	String json = gson.toJson(s);
         return null;
     }
 
