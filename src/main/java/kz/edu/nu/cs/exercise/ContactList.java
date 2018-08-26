e7e1e4fe34eefb94bbb8bcf7c6fa5e1bc39db8a8
siskin-se7en <yergaliyevz@gmail.com>
2018-08-26 13:35:11 +0600

    Update ContactList.java

--- src/main/java/kz/edu/nu/cs/exercise/ContactList.java
+++ src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -2,6 +2,8 @@ package kz.edu.nu.cs.exercise;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class ContactList {
     private List<MyContact> list;
@@ -12,7 +14,9 @@ public class ContactList {
     
     public static ContactList MakeContactList(String s) {
         // Complete this method, use Gson
-        return null;
+        Gson gson = new Gson();
+        ContactList g = gson.fromJson(s, ContactList.class);
+        return g;
     }
     
     public void addContact(MyContact c) {
@@ -26,12 +30,12 @@ public class ContactList {
     public MyContact getFirstContact() {
         // complete this method
         // return correct value
-        return null;
+        return list.get(0);
     }
     
     public int getSize() {
         // complete this method
         // return correct value
-        return 0;
+        return list.size();
     }
 }
