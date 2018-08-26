d5cc3e20c044c9238e41770afb226417e72e07fd
dianazhanseit <32613789+dianazhanseit@users.noreply.github.com>
2018-08-26 18:08:13 +0600

    Add files via upload

---
+++ src/MyContact.java
@@ -0,0 +1,46 @@
+package kz.edu.nu.cs.exercise;
+
+import com.google.gson.Gson;
+
+public class MyContact {
+    private String name;
+    private int age;
+    private String number;
+
+    public MyContact(String name, int age, String number) {
+        this.name = name;
+        this.age = age;
+        this.number = number;
+    }
+
+    public static MyContact MakeMyContact(String s) {
+        Gson gson = new Gson();
+        MyContact g = gson.fromJson(s, MyContact.class);
+        return g;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
+
